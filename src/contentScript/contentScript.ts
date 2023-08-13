96c7516c605e0f8460ea17cce4a9816251c6247c
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-08-13 14:11:51 +0800

    feat: send tweets to server

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,1 +1,51 @@
-// TODO: content script
+import { sendTweetToServer } from "../utils/api";
+
+const detectNewTweets = async (): Promise<void> => {
+    const elements = document.getElementsByClassName(
+        "css-1dbjc4n r-1igl3o0 r-qklmqi r-1adg3ll r-1ny4l3l"
+    );
+
+    for (let index = 0; index < elements.length; index++) {
+        const tweet = elements[index] as HTMLDivElement;
+
+        if (tweet.hasAttribute("data-tweet-processed")) {
+            continue;
+        }
+
+        try {
+            tweet.setAttribute("data-tweet-processed", "true");
+
+            // TODO
+            // check if tweet is election-related before sending to server
+
+            const result = await sendTweetToServer(tweet.textContent);
+            if (result === 1) {
+                // TODO
+                // hide tweet instead of just changing the background color
+                tweet.style.backgroundColor = "red";
+
+                // updateDetectedTweetsCount();
+            }
+        } catch (err) {}
+    }
+};
+
+const scrollFunction = (): void => {
+    setTimeout(() => {
+        window.scrollBy(10, 10);
+    }, 1000);
+};
+
+const enableExtension = (): void => {
+    document.addEventListener("DOMContentLoaded", detectNewTweets);
+    window.addEventListener("load", scrollFunction);
+    window.addEventListener("scroll", detectNewTweets);
+};
+
+enableExtension();
+
+const disableExtension = (): void => {
+    document.removeEventListener("DOMContentLoaded", detectNewTweets);
+    window.removeEventListener("load", scrollFunction);
+    window.removeEventListener("scroll", detectNewTweets);
+};
