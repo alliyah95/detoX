df97dee3f47448f49ca37304db276d709484bf90
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-08-20 14:59:46 +0800

    feat: create `Toggle` component

--- src/popup/components/Header.tsx
+++ src/popup/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Toggle from "./Toggle";
 
 const Header: React.FC<{}> = () => {
     return (
         <div className="header__wrapper bg-white text-center">
             <h1 className="header__title text-blue">detoX</h1>
+            <Toggle />
         </div>
     );
 };
---
+++ src/popup/components/Toggle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Toggle: React.FC<{}> = () => {
+    return (
+        <div className="checkbox_wrapper">
+            <input type="checkbox" id="toggle-switch" />
+        </div>
+    );
+};
+
+export default Toggle;
