af5bcdeed8466066d71a2457b8bc5a4cd6ccf5cf
Alliyah Joyce Sarip <saripalliyahjoyce@gmail.com>
2023-09-18 20:28:33 +0800

    refactor: remove commented-out code

--- src/background/background.ts
+++ src/background/background.ts
@@ -25,22 +25,3 @@ const handleTweetProcessingState = (message) => {
 
 chrome.runtime.onMessage.addListener(handleTweetProcessingState);
 chrome.action.setBadgeBackgroundColor({ color: [201, 48, 48, 255] });
-
-// TODO change UTC hours to 4
-// const utcTime = new Date();
-// utcTime.setUTCHours(2);
-// utcTime.setUTCMinutes(23);
-// utcTime.setUTCSeconds(0);
-// utcTime.setUTCMilliseconds(0);
-
-// chrome.alarms.create("resetDailyCount", {
-//     when: utcTime.getTime(),
-// });
-
-// chrome.alarms.onAlarm.addListener((alarm) => {
-//     if (alarm.name === "resetDailyCount") {
-//         resetDailyTweetCount().then(() => {
-//             console.log("Daily tweet count has been reset!");
-//         });
-//     }
-// });
